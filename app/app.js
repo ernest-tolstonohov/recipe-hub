cc9a76cd4ab3ba76966637d2ddbfb086bb9f2c1e
Hajar Natiq <evahajar2004@gmail.com>
2026-03-19 00:55:35 +0100

    Add base layout, navbar, footer and CSS styles

--- app/app.js
+++ app/app.js
@@ -1,48 +1,30 @@
-// Import express.js
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
-// Create express app
 var app = express();
 
-// Add static files location
-app.use(express.static("static"));
+// Tell the app to use Pug for pages
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-// Get the functions in the db.js file to use
+// Allow the app to read form data
+app.use(express.urlencoded({ extended: true }));
+
+// Add static files location (CSS, images)
+app.use(express.static(path.join(__dirname, "../static")));
+
+// Get the database
 const db = require('./services/db');
 
-// Create a route for root - /
+// Home route - just for testing for now
 app.get("/", function(req, res) {
     res.send("Hello world!");
 });
 
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+// Start the server
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
-
-// Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
-});
+module.exports = app;
