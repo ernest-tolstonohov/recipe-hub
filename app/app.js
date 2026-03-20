4ecde30cf4b966ce045b8555aed6dcdd69dd2939
Hajar Natiq <evahajar2004@gmail.com>
2026-03-20 03:20:41 +0100

    Fix session user global and clean up app.js

--- app/app.js
+++ app/app.js
@@ -11,8 +11,14 @@ app.set("views", path.join(__dirname, "views"));
 // Allow the app to read form data
 app.use(express.urlencoded({ extended: true }));
 
-// Add static files location (CSS, images)
+// Add static files location
 app.use(express.static(path.join(__dirname, "../static")));
+
+// Make user available to ALL pages automatically
+app.use((req, res, next) => {
+    res.locals.user = req.session ? req.session.user || null : null;
+    next();
+});
 
 // Get the database
 const db = require('./services/db');
@@ -20,14 +26,16 @@ const db = require('./services/db');
 // Import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
+
 // Use routes
 app.use('/', authRoutes);
 app.use('/', userRoutes);
+
 // Home route
 app.get("/", function(req, res) {
-    const user = req.session ? req.session.user : null;
-    res.render('index', { user: user });
+    res.render('index', { user: res.locals.user });
 });
+
 // Start the server
 app.listen(3000, function() {
     console.log("Server running on port 3000");
